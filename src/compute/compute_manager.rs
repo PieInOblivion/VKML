9b57a7ac97f1dcba6cad0a188fcf0405cf30cf8a
PieInOblivion <14125393+PieInOblivion@users.noreply.github.com>
2026-01-09 06:23:11 +1100

    fix: optimise batch loading to handle CPU-backed tensors without unnecessary allocation

--- src/compute/compute_manager.rs
+++ src/compute/compute_manager.rs
@@ -813,10 +813,14 @@ struct BatchLoadParams<'a> {
 }
 
 zp_define_task_fn!(batch_load_task, BatchLoadParams, |params| {
-    params
-        .compute_manager
-        .tensor_write(params.tensor_id)
-        .write(&params.batch.read());
+    let dest = params.compute_manager.tensor_write(params.tensor_id);
+
+    // if batch is CPU backed, avoid allocating via batch.read()
+    if params.batch.is_cpu() {
+        dest.write(params.batch.get_cpu_memory_slice_or_panic());
+    } else {
+        dest.write(&params.batch.read());
+    }
 });
 
 struct BatchCopyParams<'a> {
